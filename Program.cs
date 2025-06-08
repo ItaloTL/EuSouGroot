afae4efaf02008ffcb2f5b7bb7cfdb9172afce56
Italo Trancoso Lopes <itltrancoso@gmail.com>
2025-06-08 09:44:05 -0300

    Tarefa EuSouGroot concluida

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿string resposta = " Eu sou Groot";
+string pergunta = "";
+Console.Clear();
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.WriteLine(@"--- Groot Chatbot ---");
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("Olá esse é meu amigo Groot, converse com ele!");
+Console.WriteLine("Assim que ficarem sem assunto apeans digite tchau");
+Console.ResetColor();
+
+while (pergunta != "TCHAU")
+{
+    Console.Write("Pergunta:   "); pergunta = Console.ReadLine()!.ToUpper();
+    if (pergunta != "tchau")
+    {
+    Console.WriteLine($"     Resposta: {resposta}.");}
+
+    else if (pergunta == "tchau")
+    {
+        Console.WriteLine($"     Resposta:{resposta}!");
+    }
+}
